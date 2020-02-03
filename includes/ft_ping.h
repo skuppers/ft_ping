356bf29b7a61a6c176c542bf2fa346acbc695223
skuppers <sebastien.kuppers@gmail.com>
2020-02-03 17:24:24 +0100

    Initial push

---
+++ includes/ft_ping.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ping.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skuppers <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/03 15:13:23 by skuppers          #+#    #+#             */
+/*   Updated: 2020/02/03 16:36:42 by skuppers         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PING_H
+# define FT_PING_H
+
+# include <signal.h>
+# include <sys/socket.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <time.h>
+# include <netdb.h>
+
+#endif
