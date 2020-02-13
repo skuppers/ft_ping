6ab8b96ad764fb62b1e7f978c70d9ca5ded7c12c
skuppers <sebastien.kuppers@gmail.com>
2020-02-13 14:28:55 +0100

    Statistics update

--- src/display.c
+++ src/display.c
@@ -6,7 +6,7 @@
 /*   By: skuppers <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 14:05:02 by skuppers          #+#    #+#             */
-/*   Updated: 2020/02/13 14:19:09 by skuppers         ###   ########.fr       */
+/*   Updated: 2020/02/13 14:28:50 by skuppers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	print_resolve(t_data *param)
 void	print_ping(t_data *param, t_icmppacket *pkt, t_timer *t)
 {
 	(void)pkt;
-	printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%f ms\n",
+	printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.2f ms\n",
 					param->pkt_size, param->hostname,
 					pkt->header.un.echo.sequence, param->ttl, t->rtt_sec);
 }
@@ -32,9 +32,9 @@ void	print_stats(t_data *param)
 
 	printf("%d packets transmitted, %d packets received, %.3f%% packet loss\n",
 			param->pkt_send, param->pkt_recvd,
-			((double)(param->pkt_recvd / param->pkt_send) * 100));
+			(100 - ((double)(param->pkt_recvd / param->pkt_send) * 100)));
 
-	printf("round-trip min/avg/max/stddev = %.3f / %.3f / %.3f / %.3f ms\n",
+	printf("round-trip min/avg/max/stddev = %.3f / %.3f / %.3f / %.2f ms\n",
 					param->rtt_min, 0.420f,
 					param->rtt_max, 0.420f);
 }
