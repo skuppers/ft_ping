63d3b9996e535cf551dadc0df3c65d309e41a1d9
skuppers <sebastien.kuppers@gmail.com>
2020-02-13 12:26:58 +0100

    Working on statistics

--- src/ft_ping.c
+++ src/ft_ping.c
@@ -6,7 +6,7 @@
 /*   By: skuppers <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/07 08:22:15 by skuppers          #+#    #+#             */
-/*   Updated: 2020/02/13 12:25:33 by skuppers         ###   ########.fr       */
+/*   Updated: 2020/02/13 12:26:54 by skuppers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void	ping_loop(t_data *param, int socket, t_timer *timer)
 
 		print_ping(param, icmp_pkt, timer);
 
-				update_statistics(param, timer);
+		update_statistics(param, timer);
 		++sequence;
 
 		ping_timer(1);
